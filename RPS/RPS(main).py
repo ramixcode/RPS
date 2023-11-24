0bcce89f7daffd1ea241e1158b323dd9df2e243f
Ramix <113880900+ramixcode@users.noreply.github.com>
2023-11-24 16:23:10 +0330

    Update RPS(main).py
    improvement project.

--- RPS/RPS(main).py
+++ RPS/RPS(main).py
@@ -1,266 +1,85 @@
 import random
 
-
-list = ['Rock','Paper','scissor']
-
+list = ['Rock','Paper','Scissor']
 player1_wins = 0
 player1_lose = 0
-
 name = 0 
+check_name = 0
 
+def underline(func):
+    def wrapper():
+        print(f"Robot selection : {random_entekhab}")
+        print("---------------------------")
+        func()
+        print("---------------------------")
+        print("Your statistics")
+        print(f"Your winnings = {player1_wins}")
+        print(f"Your losses = {player1_lose}")
+    return wrapper
 
+@underline
+def win():
+    global player1_lose
+    global player1_wins
+    print('You won the game')
+    player1_wins =+ 1
+    player1_lose = player1_lose  
+
+@underline
+def lose():
+    global player1_lose
+    global player1_wins
+    print('You lose the game')
+    player1_wins = player1_wins
+    player1_lose =+ 1
+
+@underline
+def draw():
+    global player1_lose
+    global player1_wins
+    print('You have draw the game shodi')
+    player1_wins = player1_wins
+    player1_lose = player1_lose 
 
 while True:
     try:
-        if name == 0:
-            player1_name = input("Enter player 1 name :")
+        if check_name == 0:
+            if name == 0:
+                player1_name = input("Enter player 1 name :")
 
-            name =+ 1
+                name =+ 1
 
-        entekhab = int(input('entkhab kon\n1)Rock\n2)Paper\n3)scissor\n===>'))
+        game_round = input(int("Enter the number of game rounds:"))
+        for i in range(0, game_round):
+            entekhab = int(input('entkhab kon\n1)Rock\n2)Paper\n3)Scissor\n===>'))
 
-        random_entekhab = random.choice(list)
+            random_entekhab = random.choice(list)
 
-        if entekhab == 1 and random_entekhab == 'Rock':
-            print(f"Robot selection : {random_entekhab}")
-            print("---------------------------")
-            print('You have tied the game shodi')
-            print('---------------------------')
+            if entekhab == 1:
+                if random_entekhab == 'Rock':
+                    draw()
+                elif random_entekhab == 'Paper':
+                    lose()         
+                elif random_entekhab == 'Scissor':
+                    win()
 
-            player1_wins = player1_wins
-            player1_lose = player1_lose   
+            if entekhab == 2:
+                if random_entekhab == 'Rock':
+                    win()
+                elif random_entekhab == 'Paper':
+                    draw()
+                elif random_entekhab == 'Scissor':
+                    lose()
 
-            print("Your statistics")
-            print(f"Your winnings = {player1_wins}")
-            print(f"Your losses = {player1_lose}")
-
-            name_check = int(input(
-                "are you need change your name?\nAlert if you change your name all statistics = 0\n1)Yes\n2)No\n===>"
-                ))
-            if name_check == 1:
-                name = 0
-                player1_wins = 0
-                player1_lose = 0
-                continue
-            else:
-                continue                    
-            
-            
-
-        if entekhab == 1 and random_entekhab == 'Paper':
-            print(f"Robot selection : {random_entekhab}")
-            print("---------------------------")
-            print('You lose the game')
-            print('---------------------------')
-            player1_wins = player1_wins
-            player1_lose =+ 1
-            
-            print("Your statistics")
-            print(f"Your winnings = {player1_wins}")
-            print(f"Your losses = {player1_lose}")     
-
-            name_check = int(input(
-                "are you need change your name?\nAlert if you change your name all statistics = 0\n1)Yes\n2)No\n===>"
-                ))
-            if name_check == 1:
-                name = 0
-                player1_wins = 0
-                player1_lose = 0
-                continue                     
-            
-            else:
-                continue           
-            
-
-        if entekhab == 1 and random_entekhab == 'scissor':
-            print(f"Robot selection : {random_entekhab}")
-            print("---------------------------")
-            print('You won the game')
-            print('---------------------------')
-            player1_wins =+ 1
-            player1_lose = player1_lose        
-            
-            print("Your statistics")
-            print(f"Your winnings = {player1_wins}")
-            print(f"Your losses = {player1_lose}")      
-
-            name_check = int(input(
-                "are you need change your name?\nAlert if you change your name all statistics = 0\n1)Yes\n2)No\n===>"
-                ))
-            if name_check == 1:
-                name = 0
-                player1_wins = 0
-                player1_lose = 0
-                continue      
-            
-            else:
-                continue            
-
-        if entekhab == 1 and random_entekhab == 'Rock':
-            print(f"Robot selection : {random_entekhab}")
-            print("---------------------------")
-            print('You have tied the game shodi')
-            print('---------------------------')
-            player1_wins = player1_wins
-            player1_lose = player1_lose
-
-            name_check = int(input(
-                "are you need change your name?\nAlert if you change your name all statistics = 0\n1)Yes\n2)No\n===>"
-                ))
-            if name_check == 1:
-                name = 0
-                player1_wins = 0
-                player1_lose = 0
-                continue      
-            
-            else:
-                continue    
-    
-        if entekhab == 2 and random_entekhab == 'Rock':
-            print(f"Robot selection : {random_entekhab}")
-            print("---------------------------")
-            print('You won the game')
-            print('---------------------------')
-            player1_wins =+ 1
-            player1_lose = player1_lose
-            
-            print("Your statistics")
-            print(f"Your winnings = {player1_wins}")
-            print(f"Your losses = {player1_lose}")  
-
-            name_check = int(input(
-                "are you need change your name?\nAlert if you change your name all statistics = 0\n1)Yes\n2)No\n===>"
-                ))
-            if name_check == 1:
-                name = 0
-                player1_wins = 0
-                player1_lose = 0
-                continue      
-            
-            else:
-                continue
-
-        if entekhab == 2 and random_entekhab == 'Paper':
-            print(f"Robot selection : {random_entekhab}")
-            print("---------------------------")
-            print('You have tied the game shodi')
-            print('---------------------------')
-            player1_wins = player1_wins
-            player1_lose = player1_lose
-            
-            print("Your statistics")
-            print(f"Your winnings = {player1_wins}")
-            print(f"Your losses = {player1_lose}")
-
-            name_check = int(input(
-                "are you need change your name?\nAlert if you change your name all statistics = 0\n1)Yes\n2)No\n===>"
-                ))
-            if name_check == 1:
-                name = 0
-                player1_wins = 0
-                player1_lose = 0
-                continue      
-            
-            else:
-                continue
-
-        if entekhab == 2 and random_entekhab == 'scissor':
-            print(f"Robot selection : {random_entekhab}")
-            print("---------------------------")
-            print('You lose the game')
-            print('---------------------------')
-            player1_wins = player1_wins
-            player1_lose =+ 1
-            
-            print("Your statistics")
-            print(f"Your winnings = {player1_wins}")
-            print(f"Your losses = {player1_lose}")  
-
-            name_check = int(input(
-                "are you need change your name?\nAlert if you change your name all statistics = 0\n1)Yes\n2)No\n===>"
-                ))
-            if name_check == 1:
-                name = 0
-                player1_wins = 0
-                player1_lose = 0
-                continue      
-            
-            else:
-                continue
-
-        if entekhab == 3 and random_entekhab == 'Rock':
-            print(f"Robot selection : {random_entekhab}")
-            print("---------------------------")
-            print('You lose the game')
-            print('---------------------------')
-            player1_wins = player1_wins
-            player1_lose =+ 1
-            
-            print("Your statistics")
-            print(f"Your winnings = {player1_wins}")
-            print(f"Your losses = {player1_lose}")     
-
-            name_check = int(input(
-                "are you need change your name?\nAlert if you change your name all statistics = 0\n1)Yes\n2)No\n===>"
-                ))
-            if name_check == 1:
-                name = 0
-                player1_wins = 0
-                player1_lose = 0
-                continue      
-            
-            else:
-                continue
-
-        if entekhab == 3 and random_entekhab == 'Paper':
-            print(f"Robot selection : {random_entekhab}")
-            print("---------------------------")
-            print('You won the game')
-            print('---------------------------')
-            player1_wins =+ 1
-            player1_lose = player1_lose
-            
-            print("Your statistics")
-            print(f"Your winnings = {player1_wins}")
-            print(f"Your losses = {player1_lose}")  
-
-            name_check = int(input(
-                "are you need change your name?\nAlert if you change your name all statistics = 0\n1)Yes\n2)No\n===>"
-                ))
-            if name_check == 1:
-                name = 0
-                player1_wins = 0
-                player1_lose = 0
-                continue      
-            
-            else:
-                continue
-
-        if entekhab == 3 and random_entekhab == 'scissor':
-            print(f"Robot selection : {random_entekhab}")
-            print("---------------------------")
-            print('You have tied the game shodi')
-            print('-------------------------')
-            player1_wins = player1_wins
-            player1_lose = player1_lose
-            
-            print("Your statistics")
-            print(f"Your winnings = {player1_wins}")
-            print(f"Your losses = {player1_lose}")     
-
-            name_check = int(input(
-                "are you need change your name?\nAlert if you change your name all statistics = 0\n1)Yes\n2)No\n===>"
-                ))
-            if name_check == 1:
-                name = 0
-                player1_wins = 0
-                player1_lose = 0
-                continue      
-            
-            else:
-                continue
-
-    except Exception as error:
+            if entekhab == 3:
+                if random_entekhab == 'Rock':
+                    lose()
+                elif random_entekhab == 'Paper':
+                    win()
+                elif random_entekhab == 'Scissor':
+                    draw()
+ 
+    except:
         print('''
         Your text must be a number.
         If you are in the game mode selection stage, the number must be one or two.
